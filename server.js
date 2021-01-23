66e5a67bd79ad24d3bb6bcf1292f7137a287416f
zaks2318 <68169592+zaks2318@users.noreply.github.com>
2021-01-22 17:58:18 -0800

    Update server.js
    edit the method on the require

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-  if (req.url == '/users'){
+  if (req.method == 'GET' && req.url == '/users'){  //When the require method is GET and  URL with /users response the list of name.
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<html><head><title>SomeTitle</title></head><body><ul><li>Owen</li><li>Kan</li><li>Wen</li><li>Cindy</li><li>Bobby</li></ul></body></html>')
     res.end();
   }
-  else if(req.url == '/work'){
+  else if(req.method == 'POST' && req.url == '/work'){ //When the require method is POST and URL with /work response the json message.
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify({message: "This is my first http OMG so cool!!"}));
     res.end();
